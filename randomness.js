f71fa7ca7f6ef39c735171fb94394b992176c9e3
devoneASC6 <nintendanime02@gmail.com>
2019-07-11 11:57:13 -0400

    banana

---
+++ randomness.js
@@ -0,0 +1,36 @@
+///////////////////////
+// GLOBAL VARIABLES //
+/////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Madhi",
+    "Harry",
+    "Sean",
+    "Robert",
+]
+//////////////////////////////
+// END OF GLOBAL VARIABLES //
+////////////////////////////
+
+function randomNumber(){
+    const startingnumber = Math.random();
+    console.log(startingnumber * 21);
+}
+randomNumber();
+
+function randomLetter(){
+    const alphabet = Math.random
+    
+}
+
